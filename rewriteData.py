6c23e38914e5f11b87af7954830ed69ebf58b118
David Thaler <davidthaler@gmail.com>
2015-06-21 13:35:13 -0700

    added doc to runModel.py and rewriteData.py

--- rewriteData.py
+++ rewriteData.py
@@ -1,3 +1,13 @@
+"""
+This script transforms the libsvm-format data from Kaggle into numpy/scipy arrays.
+It should be run once, before running any of the other code.
+
+project: Kaggle WISE 2014 Greek Media competition
+author: David Thaler
+"""
+
 import util
+print 'transforming training data...'
 rewriteTrain()
+print 'transoforming test data...'
 rewriteTest()
